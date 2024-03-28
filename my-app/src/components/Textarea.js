11ee78858dba6ab4feae976d5d90d66e764b0e77
Tarunkumar-33 <tarunkv169@gmail.com>
2024-03-28 16:02:06 +0530

    creating Textutils with wordcount,charcount,preview

--- my-app/src/components/Textarea.js
+++ my-app/src/components/Textarea.js
@@ -1,12 +1,18 @@
 import React,{useState} from 'react'
 
 export default function Textarea(props) {
-    const[text,settext]=useState("this is something here")
+    const[text,settext]=useState("")
 
     const handleupclick=()=>{
         console.log("button is clicked for uppercase" + text);
         const newtext = text.toUpperCase();
         settext(newtext);
+    }
+
+    const handledownclick=()=>{
+      console.log("button2 is clicked for lowercase");
+      const newtext = text.toLowerCase();
+      settext(newtext);
     }
 
     const handleupchange=(event)=>{
@@ -16,13 +22,25 @@ export default function Textarea(props) {
 
     return (
     <>
-      <div>
+      <div className='container my-3'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
          <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleupchange}></textarea>
         </div>
-        <button className='button1' id='uppercase' onClick={handleupclick}>Convert to uppercase</button>
+        <button className='button1 mx-3' id='uppercase' onClick={handleupclick}>Convert to uppercase</button>
+
+        <button className='button2 mx-3' id='lowercase' onClick={handledownclick}>Convert to lowercase</button>
      </div>
+
+     <div className="container my-3">
+      <h1>text details</h1>
+      <p>{text.split(" ").length} wordcount and {text.length} charcount</p>
+      <p>it takes {0.008*text.length}min to read the content</p>
+      <h2>PREVIEW</h2>
+      <p>{text}</p>
+     </div>
+
+     
     </>
   )
 }
