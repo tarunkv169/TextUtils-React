6b5e7858e8341028f8bd280dec2d12003ada6c2e
Tarunkumar-33 <tarunkv169@gmail.com>
2024-03-30 02:40:48 +0530

    setting dark mode in textutils

--- my-app/src/components/Textarea.js
+++ my-app/src/components/Textarea.js
@@ -20,24 +20,45 @@ export default function Textarea(props) {
         settext(event.target.value);
     }
 
+    const handleclear=()=>{
+        settext("");
+    }
+
+    const handlecopy=()=>{
+        var text = document.getElementById("mybox");
+        text.select();
+        navigator.clipboard.writeText(text.value)
+    }
+    
+    const speak_lang=()=>{
+      let speech = new SpeechSynthesisUtterance();
+      speech.text = text;
+      window.speechSynthesis.speak(speech);
+    }
+
     return (
     <>
-      <div className='container my-3'>
-        <h1>{props.heading}</h1>
+      <div className='container my-3' style={{color: props.mode==='light'?'black':'white'}}>
+        <h1 style={{color: props.mode==='light'?'black':'white'}}>{props.heading}</h1>
         <div className="mb-3">
-         <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleupchange}></textarea>
+         <textarea className="form-control" id="mybox" rows="8" value={text} onChange={handleupchange} style={{backgroundColor: props.mode==='light'?'white':'#43966f',color: props.mode==='light'?'black':'white'}}></textarea>
         </div>
-        <button className='button1 mx-3' id='uppercase' onClick={handleupclick}>Convert to uppercase</button>
+        <button className='btn btn-primary mx-2' id='uppercase' onClick={handleupclick}    style={{backgroundColor: props.mode==='light'?'blue':'#43966f'}}>Convert to uppercase</button>
 
-        <button className='button2 mx-3' id='lowercase' onClick={handledownclick}>Convert to lowercase</button>
+        <button className='btn btn-primary mx-2' id='lowercase' onClick={handledownclick}    style={{backgroundColor: props.mode==='light'?'blue':'#43966f'}}>Convert to lowercase</button>
+        <button className='btn btn-primary mx-2' id='clear' onClick={handleclear}    style={{backgroundColor: props.mode==='light'?'blue':'#43966f'}}>Clear</button>
+        <button className='btn btn-primary mx-2' id='copy' onClick={handlecopy}    style={{backgroundColor: props.mode==='light'?'blue':'#43966f'}}>Copy text</button>
+        <button className='btn btn-primary' onClick={speak_lang}    style={{backgroundColor: props.mode==='light'?'blue':'#43966f'}}>Speak</button>
+
+       
      </div>
 
-     <div className="container my-3">
+     <div className="container my-3" style={{color: props.mode==='light'?'black':'white'}}>
       <h1>text details</h1>
       <p>{text.split(" ").length} wordcount and {text.length} charcount</p>
       <p>it takes {0.008*text.length}min to read the content</p>
       <h2>PREVIEW</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter something in textbox above to preview that here"}</p>
      </div>
 
      
