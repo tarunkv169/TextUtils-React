6685b84414c349b3286a178eeafa5b812d928cca
Tarunkumar-33 <tarunkv169@gmail.com>
2024-03-30 02:42:20 +0530

    setting backgroundColor in darkmode in textutils

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,43 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+// import About from './components/About';
+
+// import { StyleSheet, Text, View } from 'react-native'
+import React,{ useState } from 'react';
+
+
+// const styles = StyleSheet.create({})
 function App() {
+
+
+  const [mode,setMode] = useState('light');
+
+  const togglemode=()=>{
+    
+      if(mode==='dark')
+      {                                               // always remmeber while func call onclick={---},style={mystyle} etc 
+        setMode('light'); 
+        document.body.style.backgroundColor='white'                               // for setmode('---')
+      }
+      else{
+        setMode('dark');
+        document.body.style.backgroundColor='#0a3622'                               // for setmode('---')
+      }
+    
+  }
+
   return (
     <>
-      <Navbar/>
+      <Navbar title='textutils' mode={mode} togglemode={togglemode}/>
+
       <div className='container mb-3'>
-         <Textarea heading="Enter text here to analyze"/>
+         <Textarea heading="Enter text here to analyze"   mode={mode} togglemode={togglemode} />
       </div>
+{/* 
+      <div className='container my-3'>
+          <About/>
+      </div> */}
     </>
   );
 }
