83ddc394d0b0135d42f9aea7e5b0fdc2502f92c6
Tarunkumar-33 <tarunkv169@gmail.com>
2024-03-31 16:45:45 +0530

    react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,63 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'; 
+import Alerts from './components/Alerts';
 import Textarea from './components/Textarea';
-// import About from './components/About';
+import About from './components/About';
 
 // import { StyleSheet, Text, View } from 'react-native'
 import React,{ useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 // const styles = StyleSheet.create({})
 function App() {
-
-
   const [mode,setMode] = useState('light');
+  const [alerttt,setalerttt] = useState(null);
+
+  const showalert=(msg,type)=>{
+      setalerttt({
+        msg: msg,
+        type: type
+      })
+
+      setTimeout(()=>{
+        setalerttt(null)
+      },1500)
+  }
 
   const togglemode=()=>{
-    
       if(mode==='dark')
       {                                               // always remmeber while func call onclick={---},style={mystyle} etc 
         setMode('light'); 
-        document.body.style.backgroundColor='white'                               // for setmode('---')
+        document.body.style.backgroundColor='#cccaca'; 
+        showalert(" Dark mode disabled","success");                            // for setmode('---')
       }
       else{
         setMode('dark');
-        document.body.style.backgroundColor='#0a3622'                               // for setmode('---')
+        document.body.style.backgroundColor='black';                               // for setmode('---')
+        showalert(" Dark mode enabled","success");                            // for setmode('---')
       }
-    
   }
+
+  
 
   return (
     <>
-      <Navbar title='textutils' mode={mode} togglemode={togglemode}/>
-
+    <Router>
+      <Navbar title='Textutils' mode={mode} togglemode={togglemode} />
+      <Alerts  alert={alerttt} />
       <div className='container mb-3'>
-         <Textarea heading="Enter text here to analyze"   mode={mode} togglemode={togglemode} />
+        <Routes>
+          <Route element={<About/>} path="/About"></Route>
+          <Route element={<Textarea  text_alert={showalert} heading="Enter text here to analyze"   mode={mode} togglemode={togglemode} />} path="/"></Route>
+        </Routes>
       </div>
-{/* 
-      <div className='container my-3'>
-          <About/>
-      </div> */}
+    </Router>
     </>
   );
 }
