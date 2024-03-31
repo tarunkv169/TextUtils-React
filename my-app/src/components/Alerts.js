c9a956c6787ebc8fb8a049a40c9c7e3ea51e9e4f
Tarunkumar-33 <tarunkv169@gmail.com>
2024-03-31 16:51:59 +0530

    Alert component

---
+++ my-app/src/components/Alerts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Alerts(props) {
+
+    const capitalize=(word)=>{
+        const text=word.toLowerCase();
+        return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+  return (
+    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+     <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
+     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+   </div>
+  )
+}
